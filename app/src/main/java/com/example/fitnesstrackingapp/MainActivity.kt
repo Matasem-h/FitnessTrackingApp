2c901fd8250675c8cac93dcfd8e879827813b4bd
Matasem <Matasem.h97@gmail.com>
2025-06-28 17:59:09 +0300

    sss

--- app/src/main/java/com/example/fitnesstrackingapp/MainActivity.kt
+++ app/src/main/java/com/example/fitnesstrackingapp/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.example.fitnesstrackingapp
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.ActionBarDrawerToggle
+import android.os.Bundle
+import android.content.Intent
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+    private lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // You can write your screen logic here
+
+        
     }
 }
