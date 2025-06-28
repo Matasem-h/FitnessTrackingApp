379b3c30fd15fa9998b26cbd24972129f65cfdf3
Matasem <Matasem.h97@gmail.com>
2025-06-28 21:52:34 +0300

    sss

---
+++ app/src/main/java/com/example/fitnesstrackingapp/ProgressViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fitnesstrackingapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProgressViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_progress_display)
+    }
+}
