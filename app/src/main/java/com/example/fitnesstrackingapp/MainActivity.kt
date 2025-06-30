c5e0c4454bbee96ecc67368e84041606c009dfda
Matasem <Matasem.h97@gmail.com>
2025-06-30 23:14:51 +0300

    sss

--- app/src/main/java/com/example/fitnesstrackingapp/MainActivity.kt
+++ app/src/main/java/com/example/fitnesstrackingapp/MainActivity.kt
@@ -8,13 +8,13 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import androidx.core.content.ContextCompat
 import java.io.DataInput
 import com.example.fitnesstrackingapp.DataInputActivity
 import com.example.fitnesstrackingapp.ProgressViewActivity
 import com.example.fitnesstrackingapp.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navView: NavigationView
     private lateinit var toggle: ActionBarDrawerToggle
@@ -22,7 +22,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.top_toolbar)
         setSupportActionBar(findViewById(R.id.top_toolbar))
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
         drawerLayout = findViewById(R.id.drawer_layout)
         navView = findViewById(R.id.nav_view)
