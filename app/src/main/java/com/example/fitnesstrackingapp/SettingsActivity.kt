cc79ce425af7fcc6a7b950e806c5fbb22de2649b
Matasem <Matasem.h97@gmail.com>
2025-07-07 23:02:35 +0300

    sss

--- app/src/main/java/com/example/fitnesstrackingapp/SettingsActivity.kt
+++ app/src/main/java/com/example/fitnesstrackingapp/SettingsActivity.kt
@@ -2,6 +2,18 @@ package com.example.fitnesstrackingapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.AutoCompleteTextView
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.app.DatePickerDialog
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
+import android.content.Intent
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
