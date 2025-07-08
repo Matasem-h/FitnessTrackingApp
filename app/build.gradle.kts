3420cdcdcda74c171586a6d4a6f86113670989ce
Matasem <Matasem.h97@gmail.com>
2025-07-09 00:05:10 +0300

    sss

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("kotlin-kapt") // Added for Room Annotation Processing
+    alias(libs.plugins.kotlin.kapt) // Added for Room Annotation Processing
 }
 
 android {
