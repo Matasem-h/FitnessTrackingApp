2d1d76ddde4b481959a456fd4851a2bf83fa364e
Matasem <Matasem.h97@gmail.com>
2025-07-10 22:14:40 +0300

    sss

--- app/src/main/java/com/example/fitnesstrackingapp/ProgressViewActivity.kt
+++ app/src/main/java/com/example/fitnesstrackingapp/ProgressViewActivity.kt
@@ -3,18 +3,25 @@ package com.example.fitnesstrackingapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import android.graphics.Color
+import android.util.TypedValue
+import android.view.Gravity
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import android.view.MenuItem
+import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
-
 import data.DatabaseProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.w3c.dom.Text
+
+import java.time.LocalDate
 
 class ProgressViewActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
@@ -63,6 +70,25 @@ class ProgressViewActivity : AppCompatActivity() {
         }
 
         // Page-Specific Code
+        // Day squares with ID for each
+        val container = findViewById<LinearLayout>(R.id.week_grid_container)
+        val today = LocalDate.now()
+
+        for (i in 0 until 56 step 7) {
+            val weekLayout = LinearLayout(this).apply {
+                orientation = LinearLayout.HORIZONTAL
+            }
+
+            val startOfWeek = today.minusDays(i.toLong())
+            val monthsLabel = TextView(this).apply {
+                text = startOfWeek.month.name.take(3)
+                width = 40
+                height = 48
+                gravity = Gravity.CENTER
+                setTextSize(TypedValue.COMPLEX_UNIT_SP, 12f)
+            }
+
+        // Coroutine part?
         CoroutineScope(Dispatchers.IO).launch {
             val db = DatabaseProvider.getDatabase(applicationContext)
             val entries = db.exerciseDao().getAllExercises()
@@ -70,8 +96,13 @@ class ProgressViewActivity : AppCompatActivity() {
 
             withContext(Dispatchers.Main) {
                 datesWithEntries.forEach() { dateStr ->
+
+
                     val viewId = resources.getIdentifier("square_${dateStr.replace("-", "_")}", "id", packageName)
                     val square = findViewById<TextView>(viewId)
+
+
+
                     square?.setBackgroundColor(ContextCompat.getColor(this@ProgressViewActivity, R.color.goal_box))
                 }
             }
