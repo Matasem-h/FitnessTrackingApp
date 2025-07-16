2c880a8b68e648415bd905ba3ef909a5ef21f96a
Matasem <Matasem.h97@gmail.com>
2025-07-16 11:36:39 +0300

    Updated unit tests

--- app/src/test/java/com/example/fitnesstrackingapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/fitnesstrackingapp/ExampleUnitTest.kt
@@ -7,21 +7,21 @@ class ExerciseUtilsTest {
 
     @Test
     fun testPushUpsWeightedValue() {
-        val entry = ExerciseEntry("push-ups", "10", "2025-07-16")
+        val entry = ExerciseEntry(name = "push-ups", durationOrSets = "10", date = "2025-07-16")
         val result = ExerciseUtils.getWeightedValue(entry)
         assertEquals(10, result)
     }
 
     @Test
     fun testWalkingWeightedValue() {
-        val entry = ExerciseEntry("walking", "25", "2025-07-16")
+        val entry = ExerciseEntry(name = "walking", durationOrSets = "25", date = "2025-07-16")
         val result = ExerciseUtils.getWeightedValue(entry)
-        assertEquals(5, result)
+        assertEquals(5, result) // 25 / 5
     }
 
     @Test
     fun testInvalidDuration() {
-        val entry = ExerciseEntry("cycling", "abc", "2025-07-16")
+        val entry = ExerciseEntry(name = "cycling", durationOrSets = "abc", date = "2025-07-16")
         val result = ExerciseUtils.getWeightedValue(entry)
         assertEquals(0, result)
     }
