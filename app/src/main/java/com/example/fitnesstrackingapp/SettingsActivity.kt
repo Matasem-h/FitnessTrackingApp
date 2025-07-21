071fc7ef68cb07db34cdc974574fdbb168e7d131
Matasem <Matasem.h97@gmail.com>
2025-07-21 06:28:07 +0300

    Added comments

--- app/src/main/java/com/example/fitnesstrackingapp/SettingsActivity.kt
+++ app/src/main/java/com/example/fitnesstrackingapp/SettingsActivity.kt
@@ -7,28 +7,13 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import android.view.MenuItem
-
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.util.*
 import kotlinx.coroutines.withContext
-
-
-import android.widget.AutoCompleteTextView
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.app.DatePickerDialog
-
-import java.util.Date
-
-import data.DatabaseProvider
-import data.ExerciseEntry
-
-
 
 class SettingsActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
@@ -78,14 +63,16 @@ class SettingsActivity : AppCompatActivity() {
         val resetButton = findViewById<Button>(R.id.reset_button)
 
         resetButton.setOnClickListener {
+            // Show confirmation dialog
             AlertDialog.Builder(this)
                 .setTitle("Reset Progress")
                 .setMessage("Are you sure you want to reset all your progress?")
                 .setPositiveButton("Yes") {_, _ ->
+                    // Run reset in background
                     CoroutineScope(Dispatchers.IO).launch {
                         // Access the database and delete all exercise entries
                         val db = data.DatabaseProvider.getDatabase(applicationContext)
-                        db.exerciseDao().deleteAll()
+                        db.exerciseDao().deleteAll() // Delete all entries
 
                         withContext(Dispatchers.Main) {
                             // Notify the user that all exercise data has been deleted
@@ -93,48 +80,11 @@ class SettingsActivity : AppCompatActivity() {
                         }
                     }
                 }
-                .setNegativeButton("No", null)
+                .setNegativeButton("No", null) // Do nothing on cancel
                 .show()
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
+
     // Enable navigation icon response to clicks
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return if (toggle.onOptionsItemSelected(item)) true
